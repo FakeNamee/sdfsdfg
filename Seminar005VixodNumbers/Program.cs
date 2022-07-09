23b84053d0bb7ff6b66fbfbdb54f4a7c027e18b4
Roma <rprilipko3@inbox.ru>
2022-07-09 15:47:17 +0300

    Вывод всех чисел от на выходе показывает все целые числа в промежутке от -N до N.

---
+++ Seminar005VixodNumbers/Program.cs
@@ -0,0 +1,10 @@
+﻿Console.WriteLine("Введите число: ");
+int number = Convert.ToInt32(Console.ReadLine());
+int startnumber = -number;
+while(startnumber != (number + 1)) 
+{
+    Console.Write(startnumber + ",");
+    startnumber+=1;
+}
+Console.Write("\b.");
+Console.WriteLine();
