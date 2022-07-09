362dafd98d5bcf291d1fd208df9e3710c1569521
Roma <rprilipko3@inbox.ru>
2022-07-09 14:44:13 +0300

    Задание второе на проверку квадрата

---
+++ Seminnar002/Program.cs
@@ -0,0 +1,14 @@
+﻿Console.Write("a = ");
+int numberA = Convert.ToInt32(Console.ReadLine());
+Console.Write("b = ");
+int numberB = Convert.ToInt32(Console.ReadLine());
+string result;
+if(numberA * numberA == numberB) 
+{
+  result = "да";
+}
+else 
+{
+  result = "нет";
+}
+Console.WriteLine(result);
